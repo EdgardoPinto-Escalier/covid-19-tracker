2ea81270a38f6423cd616447941e0c25ab67e50e
EdgardoPinto-Escalier <ep-marketing-media@live.com>
2020-04-12 09:33:06 +0200

    Fetched and showed data for global infected cases

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 import styles from './Cards.module.css';
 
-const Cards = (props) => {
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate }}) => {
+  if(!confirmed) {
+    return 'Loading...';
+  }
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>INFECTED</Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary">REAL DATE</Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+            </Typography>
+  <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2">Number of COVID-19 active cases</Typography>
           </CardContent>
         </Grid>
